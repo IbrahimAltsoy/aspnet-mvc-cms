0b11e704c00479f3561bb56c698b80d908562172
IbrahimAltsoy <78874266+IbrahimAltsoy@users.noreply.github.com>
2023-02-27 01:41:46 +0300

    added register template

---
+++ App.Web.Mvc/App.Web.Mvc/Controllers/ErrorPageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.Web.Mvc.Controllers
+{
+    public class ErrorPageController : Controller
+    {
+        public IActionResult ErrorNumber(int id)
+        {
+            return View();
+        }
+    }
+}
