bb6c4447d7383ad2213a0a5795685a1f72c101c7
aylmz <98480653+aylmz23@users.noreply.github.com>
2023-03-03 18:40:33 +0300

    Yedek Entity Eklendi

--- App.Data/Entity/Yedek/Services.cs
+++ App.Data/Entity/Yedek/Services.cs
@@ -11,7 +11,7 @@ namespace App.Data.Entity.Yedek
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public string Image { get; set; }
+        public string? Icon { get; set; }
 
     }
 }
