a2d6a3984ae23afc319138b8f77731fda1c7cd33
aylmz <98480653+aylmz23@users.noreply.github.com>
2023-03-05 19:24:41 +0300

    .1

--- App.Data/Entity/Setting.cs
+++ App.Data/Entity/Setting.cs
@@ -1,10 +1,19 @@
-﻿namespace App.Data.Entity
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace App.Data.Entity
 {
     public class Setting
     {
         public int Id { get; set; }
         public int? UserId { get; set; }
+
+        [Required(ErrorMessage = "{0} can not be blank!"), Column(TypeName = "VARCHAR(50)")]
         public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "{0} can not be blank!"), Column(TypeName = "VARCHAR(10)")]
         public string Value { get; set; }
 
         public User User { get; set; }
--- App.Data/Entity/User.cs
+++ App.Data/Entity/User.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,12 +11,30 @@ namespace App.Data.Entity
     public class User
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} can not be blank!"), Column(TypeName = "VARCHAR(50)")]
         public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "{0} can not be blank!"), Column(TypeName = "VARCHAR(16)")]
         public string Password { get; set; }
+
+
+        [Required(ErrorMessage = "{0} can not be blank!"), Column(TypeName = "VARCHAR(40)")]
         public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "{0} can not be blank!"), Column(TypeName = "VARCHAR(30)")]
         public string City { get; set; }
+
+
+        [Required(ErrorMessage = "{0} can not be blank!"), Column(TypeName = "VARCHAR(16)")]
         public string Phone { get; set; }
-        public string PhotoFile { get; set; }        
+
+
+        [Required(ErrorMessage = "{0} can not be blank!"), Column(TypeName ="VARCHAR(80)")]
+        public string PhotoFile { get; set; }
+
         public bool IsAdmin { get; set; }
     }
 }
