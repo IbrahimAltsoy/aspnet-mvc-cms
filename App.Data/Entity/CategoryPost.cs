27bc9a10fffacaef14f7c4f5eeefe86b6ae9b529
IbrahimAltsoy <78874266+IbrahimAltsoy@users.noreply.github.com>
2023-03-05 18:56:25 +0300

    ..

--- App.Data/Entity/CategoryPost.cs
+++ App.Data/Entity/CategoryPost.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace App.Data.Entity
+﻿namespace App.Data.Entity
 {
-    internal class CartegoryPost
+    public class CategoryPost
     {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public int PostId { get; set; }
+
+        public Post Post { get; set; }
+        public Category Category { get; set; }
     }
 }
