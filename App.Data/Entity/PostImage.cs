b8b40aec77f9005fd1c97ee9d862804f215265ab
busrakrks <115918600+busrakrks@users.noreply.github.com>
2023-03-05 19:04:04 +0300

    PostImage eklendi

---
+++ App.Data/Entity/PostImage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Data.Entity
+{
+	public class PostImage
+	{
+		public int Id { get; set; }
+		public int PostId { get; set; }
+		public string ImagePath { get; set; }
+	}
+}
