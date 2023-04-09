a9774c3d38a73132277eb084e4ec3d40f3fb5545
IbrahimAltsoy <78874266+IbrahimAltsoy@users.noreply.github.com>
2023-04-09 10:48:07 +0300

    ServiceControllerde Index te image in listelenmesi sağlandı

--- App.Web.Mvc/Areas/Admin/Controllers/ServiceController.cs
+++ App.Web.Mvc/Areas/Admin/Controllers/ServiceController.cs
@@ -1,7 +1,9 @@
 ﻿using App.Data;
 using App.Data.Entity;
+using App.Web.Mvc.Utils;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Drawing.Drawing2D;
 
 namespace App.Web.Mvc.Areas.Admin.Controllers
 {
@@ -39,12 +41,25 @@ namespace App.Web.Mvc.Areas.Admin.Controllers
         // POST: ServiceController/Create
         // POST: CategoriesController/Create
         [HttpPost]        
-        public async Task<IActionResult> Create(Service service)
+        public async Task<IActionResult> Create(Service service, IFormFile? Logo)
         {
 
-            await _context.Services.AddAsync(service);        
-            await _context.SaveChangesAsync();            
-            return RedirectToAction(nameof(Index));
+            if (ModelState.IsValid) // Model class ımız olan brand nesnesinin validasyon için koyduğumuz kurallarınıa (örneğin marka adı required-boş geçilemez gibi) uyulmuşsa
+            {
+                try
+                {
+                    service.Image = await FileHelpers.FileLoaderAsync(Logo);
+                    await _context.AddAsync(service);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch
+                {
+                    ModelState.AddModelError("", "Hata Oluştu!");
+                }
+            }
+
+            return View(service);
 
 
 
